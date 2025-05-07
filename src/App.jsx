1ecbab46c4f33a869aab6f0a08e1371bbadc8c9e
Atwijukire Ariho Seth <arihoseth18@gmail.com>
2025-05-07 22:45:08 +0100

    Worked on the Loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
+import './index.css'
 import './App.css'
 import LoadingScreen from './componets/LoadingScreen'
+import { useState } from 'react'
 
 
 function App() {
+  const[isLoaded, setIsLoaded]= useState(false);
 
   return (
     <>
-    <LoadingScreen />
+    {!isLoaded && <LoadingScreen onComplete={()=> setIsLoaded(true)} />}
+    <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+      <p>Welcome here</p>
+    </div>
      
     </>
   )
